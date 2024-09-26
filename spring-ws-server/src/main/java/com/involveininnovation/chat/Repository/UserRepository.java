08da1bb90f2d7de3052c9070174eb9cf96b456ac
MadhavKabra10 <madhavkabra57@gmail.com>
2024-09-27 00:06:10 +0530

    Find only 100 lawyers

--- spring-ws-server/src/main/java/com/involveininnovation/chat/Repository/UserRepository.java
+++ spring-ws-server/src/main/java/com/involveininnovation/chat/Repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.involveininnovation.chat.Repository;
 
 import com.involveininnovation.chat.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
 import java.util.Optional;
 import  java.util.List;
 public interface UserRepository extends MongoRepository<User,String> {
+    @Query("SELECT * FROM User LIMIT 100")
+    public List<User> findFirst100Users();
     public Optional<User> findUserByEmail(String id);
     public List<User> findUserByProfession(String id);
 }
